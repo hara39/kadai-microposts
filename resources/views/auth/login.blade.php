0cc2e78d9ca1b8a32293ef02be0667427b15388e
apple39 <apple39.release@gmail.com>
2018-11-27 07:52:38 +0000

    user login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,21 +8,21 @@
     <div class="row">
         <div class="col-md-6 col-md-offset-3">
             
-            {!! Form::open(['route' => 'login.post]) !!}
+            {!! Form::open(['route' => 'login.post']) !!}
                 <div class="form-group">
                     {!! Form::label('email', 'Email') !!}
-                    {!! Form::email('email', old('email'), ['class' => 'form-control'] !!}
+                    {!! Form::email('email', old('email'), ['class' => 'form-control']) !!}
                 </div>
                 
                 <div class="form-group">
                     {!! Form::label('password', 'Password') !!}
-                    {!! Form::password('password', ['class' => 'form-control'] !!}
+                    {!! Form::password('password', ['class' => 'form-control']) !!}
                 </div>
                 
-                {!! Form::submit('Log in', ['class' => 'btn btn-primary btn-block'] !!}
+                {!! Form::submit('Log in', ['class' => 'btn btn-primary btn-block'])!!}
             {!! Form::close() !!}
             
-            <p>New user? {link_to_route('signup.get', 'Sign up now!') !!}</p>
+            <p>New user? {!! link_to_route('signup.get', 'Sign up now!') !!}</p>
         </div>
     </div>
 
