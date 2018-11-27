a2a883660501e83a98703c50044536bb0b0e1485
apple39 <apple39.release@gmail.com>
2018-11-27 03:57:11 +0000

    top page

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+//ユーザー登録
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
+
+//ユーザー認証
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::get('login', 'Auth\LoginController@Login')->name('login.post');
+Route::get('logout', 'Auth\RegisterController@logout')->name('logout.get');
